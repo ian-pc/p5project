29594b0b8942f409ee1f1103fe1144be58714bd7
ian-pc <67145217+ian-pc@users.noreply.github.com>
2021-04-04 17:30:23 +0900

    d

---
+++ sketch.js
@@ -0,0 +1,41 @@
+let bf_url = "https://coolors.co/70d6ff-ff70a6-ff9770-ffd670-e9ff70";
+bf_url = bf_url.split("/");
+let url = bf_url[3];
+//console.log(url);
+let color_cd = [];
+
+let new_color = bf_url[3].split("-");
+
+let result_color = "";
+//console.log(new_color.length);
+
+
+
+for(let i= 0; i < new_color.length; i++) {
+  color_cd[i] = "#" + new_color[i];
+
+}
+
+
+console.log(color_cd);
+
+
+
+
+
+function setup() {
+  createCanvas(1000, 800);
+}
+
+function draw() {
+  background(220);
+  
+ // console.log("reseult:" + color_cd[3]);
+  //stroke("#99ff99");
+  
+  let c = random(color_cd);
+  fill(c);
+  ellipse(100,100,100,100);
+  
+  
+}
